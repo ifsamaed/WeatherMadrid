c9eee543224d031368945d8cae762743d5e045d0
Margaret López Calderón <ifsamaed@gmail.com>
2017-07-11 21:17:00 +0200

    Siguiendo el tutorial, he añadido a pod Alamofire para hacer una petición del tiempo, he muestra el tiempo actual

---
+++ WeatherMadrid/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  WeatherMadrid
+//
+//  Created by Margaret Lopez Calderon on 3/7/17.
+//  Copyright © 2017 Lopez, Margaret. All rights reserved.
+//
+
+import Foundation
